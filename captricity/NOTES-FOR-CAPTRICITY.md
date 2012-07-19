e66f7e22b941ac9b287957814e35b3a1780de5e2
dennis feehan <dfeehan@princeton.edu>
2012-07-19 12:56:58 -0400

    added new job for us to use in api experiments...

--- captricity/NOTES-FOR-CAPTRICITY.md
+++ captricity/NOTES-FOR-CAPTRICITY.md
@@ -3,6 +3,9 @@ Notes for captricity
 
 This file will keep track of some of our comments about Captricity and the Captricity API, in case they find them useful.
 
+* there doesn't seem to be an easy way to see all of the templates that we've created. for example, when browsing a completed job, it would be useful to be able to see and copy / edit the template that was used to create the data from the job
+
+
 
 
 
--- captricity/upload-files.py
+++ captricity/upload-files.py
@@ -32,10 +32,24 @@ client = Client(api_token)
 ## our client
 client.print_help()
 
-## figure out which documents to read...
-docs = client.read_documents()
-
 ## create a new job
+## the full survey document id is 1969
+## figured this out by hand using:
+##  jobs = client.read_jobs()
+##  job_names = [x['name'] for x in jobs]
+##  job_docids = [x['document']['id'] for x in jobs]
+
+## NB: we don't want to do this all the time!
+## created a job with id 2302 for us to
+## play around with...
+##post_data = { 'document_id' : 1969,
+##              'name' : 'api-test-job' }
+##job = client.create_jobs(post_data)
+
+
+
+
+
 
 
 
