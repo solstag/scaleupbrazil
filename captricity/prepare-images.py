7dd99b0fda4c56639e2009d0ad62cd591edce0fe
Alexandre Hannud Abdo <abdo@member.fsf.org>
2012-07-22 18:35:34 -0400

    Improve prepare-images.py so it takes the target path name as an argument.

--- captricity/prepare-images.py
+++ captricity/prepare-images.py
@@ -3,25 +3,36 @@
 # prepare-images.py
 #
 # Creates pngs from multipage pdfs and tiffs
-#
-# BUG: supposes files located in working directory
 
-import Image, os
+import Image, os, sys
 from subprocess import call
 
-pdfbundles = filter( lambda x: x.endswith('.pdf'), os.listdir('.') )
-tifbundles = filter( lambda x: x.endswith( ('.tif', '.tiff') ), os.listdir('.') )
+def main():
+  if len(sys.argv)>1:
+    try:
+      os.chdir(sys.argv[1])
+    except OSErrori as e:
+      print e
+      return
+  
+  pdfbundles = filter( lambda x: x.endswith('.pdf'), os.listdir('.') )
+  tifbundles = filter( lambda x: x.endswith( ('.tif', '.tiff') ), os.listdir('.') )
+  
+  if len(pdfbundles) == len(tifbundles) == 0 :
+    print 'Nothing to be done, quitting.'
+  
+  for bundle in tifbundles:
+    im=Image.open(bundle)
+    try:
+      while True:
+        im.rotate(270)
+        im.save( bundle[:-3] + '-' + str(im.tell()) + '.png' )
+        im.seek(im.tell()+1)
+    except EOFError:
+      pass
+  
+  for bundle in pdfbundles:
+    retcode = call([ 'convert', '-rotate', '90', bundle, bundle[:-3]+'.png' ])
 
-for bundle in tifbundles:
-  im=Image.open(bundle)
-  try:
-    while True:
-      im.rotate(270)
-      im.save( bundle[:-3] + '-' + str(im.tell()) + '.png' )
-      im.seek(im.tell()+1)
-  except EOFError:
-    pass
-
-for bundle in pdfbundles:
-  retcode = call([ 'convert', '-rotate', '90', bundle, bundle[:-3]+'.png' ])
+main()
 
