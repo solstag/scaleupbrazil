bb02bd64f5ef1cba3b7cf5b126c5cddc776c0921
Alexandre Hannud Abdo <abdo@member.fsf.org>
2012-07-24 16:27:03 -0400

    Renamed upload-scans to uploadscans. Also, now the script actually works.

--- captricity/web/uploadscans.py
+++ captricity/web/uploadscans.py
@@ -10,34 +10,31 @@ render = web.template.render('templates/')
 urls = ('/', 'index')
 app = web.application(urls, globals())
 
-class questionnaires:
-    q = ['110087','110088','110089']
-    def get(self):
-        return self.q
-    def default(self): 
-        return '110087'
-    def remove(self, value):
-        self.q.remove(value)
-
+quests = ['110087','110088','110089']
+defaultquest = '110087'
 
 class index: 
     uploadform = form.Form( 
-        form.Dropdown(name='Questionario', args=questionnaires.get(), value=questionnaires.default() ),
+        form.Dropdown(name='Questionario', args=quests, value=defaultquest),
         form.File(name='Arquivo'))
     def GET(self): 
         form = self.uploadform()
-        return render.formtest(form)
+        return render.uploadscans(form)
 
     def POST(self): 
         form = self.uploadform() 
         myinput = web.input(Arquivo={})
         if not form.validates(): 
-            return render.formtest(form)
+            return render.uploadscans(form)
         else:
             safewrite(path.expanduser('~/.scaleupbrazil/uploadforms/')+form['Questionario'].value+'.pdf',
                       myinput['Arquivo'].value)
-            questionnaires.remove(form['Questionario'].value)
-            return "Grrreat success! Questionnaire {} uploaded.<br/>".format(form['Questionario'].value)+render.formtest(form)
+            quests.remove(form['Questionario'].value)
+#            return "Grrreat success! Questionnaire {} uploaded.<br/>".format(form['Questionario'].value)
+            if quests:
+                return render.uploadscans(self.uploadform())
+            else:
+                return 'Congratulations, you are done!'
 
 if __name__=="__main__":
     web.internalerror = web.debugerror
