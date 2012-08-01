3d59065a74072a568b5bd9ccac0d402978432715
dennis feehan <dfeehan@princeton.edu>
2012-08-01 12:04:50 -0400

    started create-job, a cl tool to help creating jobs with a specific document; this is needed to create the templates for each survey path

---
+++ captricity/create-job.py
@@ -0,0 +1,38 @@
+#! /usr/bin/env python
+
+# create-job.py
+#
+# create a job with a document; this will be used as a utility in
+# creating the different templates for each possible survey path
+
+import sys
+import os
+import re
+import argparse
+import captools.api
+from captools.api import ThirdPartyApplication
+from captools.api import Client
+from captricityTransfer import *
+
+def main():
+
+  parser = argparse.ArgumentParser(description='Create a captricity job with no imagesets but a template. Useful in creating the tempaltes for the different survey paths.')
+  
+  parser.add_argument('-n', '--name', action="store",
+                      dest="job_name", default=None,
+                      help="the name of the job to create")
+  parser.add_argument('-d', '--documentid', action="store",
+                      dest="document_id", default=None,
+                      help="the id of the document/template to associate with the job")
+
+  args = parser.parse_args()
+
+  api_token=get_token()
+  client = Client(api_token)
+
+
+
+main()
+
+
+
