25bbe635b0e02fe0e0e230e116a2846ac74b26ce
dennis feehan <dfeehan@princeton.edu>
2012-09-11 12:19:53 -0400

    starting to add users and backend code to adjudicator...

--- captricity/web/adju.py
+++ captricity/web/adju.py
@@ -7,6 +7,13 @@ import web
 from web import form, safewrite
 import sys, pymongo
 
+# TODO -- 
+# next steps here:
+#   * add user login / logout (and sessions, etc)
+#   * add post method / backend code for registering diffs
+#   * flesh out db schema / strategy in more detail
+
+
 # TODO - for now, this seems to work for getting the
 # captricityTransfer library into the search path
 # (but there must be a better way?)
@@ -43,24 +50,10 @@ qid_job, iset_qid, iset_shred, qid_shred = ct.load_useful_maps()
 available_qids = qid_shred.keys()
 
 class index: 
+
     def GET(self): 
         form = self.uploadform()
         return render.uploadscans(form)
-
-#     def POST(self): 
-#         form = self.uploadform() 
-#         myinput = web.input(Arquivo={})
-#         if not form.validates(): 
-#             return render.uploadscans(form)
-#         else:
-#             safewrite(path.expanduser('~/.scaleupbrazil/uploadforms/')+form['Questionario'].value+'.pdf',
-#                       myinput['Arquivo'].value)
-#             quests.remove(form['Questionario'].value)
-# #            return "Grrreat success! Questionnaire {} uploaded.<br/>".format(form['Questionario'].value)
-#             if quests:
-#                 return render.uploadscans(self.uploadform())
-#             else:
-#                 return 'Congratulations, you are done!'
 
 class questionnaire_id: 
     def GET(self, questionnaire_id): 
@@ -71,6 +64,12 @@ class questionnaire_id:
         return render.questionnaire_test(questionnaire_id, 
                                          zip(shreds.keys(), 
                                              shreds.values()))
+
+    def POST(self, questionnaire_id):
+
+        pass
+
+        ## TODO -- fill this in
 
 class questionnaire_diffs: 
     def GET(self, questionnaire_id): 
@@ -87,6 +86,12 @@ class questionnaire_diffs:
                                               df,
                                               zip(diff_shred_qs,
                                                   diff_shred_ids))
+
+    def POST(self, questionnaire_id):
+
+        pass
+
+        ## TODO -- need to write this part...
 
 class shred_test: 
     def GET(self, shred_id): 
