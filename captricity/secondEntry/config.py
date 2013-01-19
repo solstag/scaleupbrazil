d007f5485a0e326f06fa16e165a01e5ffd3929ca
dfeehan <dfeehan@princeton.edu>
2013-01-18 21:06:42 -0500

    moved some functions to config.py from captricityTransfer.py (which we are refactoring out...)

--- captricity/secondEntry/config.py
+++ captricity/secondEntry/config.py
@@ -22,3 +22,48 @@ def get_scan_dirs(template_file="~/.scaleupbrazil/scan-directories.json"):
     sys.exit()
 
   return res
+
+def get_template_map(template_file):
+  """
+  read in the file that has the mapping from survey paths to the corresponding document/template IDs
+  """
+
+  ## TODO-EXCEPTION
+  infile = open(os.path.expanduser(template_file), 'r')
+  res = json.load(infile)
+  infile.close()
+
+  return res
+
+def get_survey_paths(svypath_file):
+  """
+  read in the file that maps questionnaire IDs to the various templates that each questionnaire
+  should use.
+  """
+
+  ## TODO-EXCEPTION
+  infile = csv.DictReader(open(os.path.expanduser(svypath_file), 'r'))  
+  ## the .csv file's first column is the row number, which we don't need
+  svy_paths = [x for x in infile]  
+  delblank = lambda x: x.pop('') and x
+  svy_paths = map(delblank, svy_paths)
+
+  return svy_paths
+
+def get_diffs(diff_file):
+  """
+  read in the file which has information about the differences between the
+  captricity and vargas results
+  """
+
+  ## TODO-EXCEPTION
+  infile = csv.DictReader(open(os.path.expanduser(diff_file), 'r'))
+
+  res = []
+
+  for row in infile:
+    res.append(row)
+
+  return res
+
+
