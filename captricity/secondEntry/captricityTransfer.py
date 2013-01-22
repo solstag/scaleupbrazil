a5bcd64d2e8615e0c3b73395c7474ef9256a6615
dfeehan <dfeehan@princeton.edu>
2013-01-22 01:12:45 -0500

    removed some stuff that was accidentally added back in during latest big merge from refactor branch...

--- captricity/secondEntry/captricityTransfer.py
+++ captricity/secondEntry/captricityTransfer.py
@@ -7,64 +7,6 @@ from captools.api import Client
 import time, datetime, dateutil.parser
 from subprocess import call
 
-def get_scan_dirs(template_file="~/.scaleupbrazil/scan-directories.json"):
-  """
-  read in the file that has the directories related to scanning survey forms
-  """
-
-  try:
-    infile = open(os.path.expanduser(template_file), 'r')
-    res = json.load(infile)
-    infile.close()
-  except Exception e:
-    print "ERROR opening scan directory configuration file ", template_file
-    sys.exit()
-
-  return res
-
-def get_template_map(template_file):
-  """
-  read in the file that has the mapping from survey paths to the corresponding document/template IDs
-  """
-
-  ## TODO-EXCEPTION
-  infile = open(os.path.expanduser(template_file), 'r')
-  res = json.load(infile)
-  infile.close()
-
-  return res
-
-def get_survey_paths(svypath_file):
-  """
-  read in the file that maps questionnaire IDs to the various templates that each questionnaire
-  should use.
-  """
-
-  ## TODO-EXCEPTION
-  infile = csv.DictReader(open(os.path.expanduser(svypath_file), 'r'))  
-  ## the .csv file's first column is the row number, which we don't need
-  svy_paths = [x for x in infile]  
-  delblank = lambda x: x.pop('') and x
-  svy_paths = map(delblank, svy_paths)
-
-  return svy_paths
-
-def get_diffs(diff_file):
-  """
-  read in the file which has information about the differences between the
-  captricity and vargas results
-  """
-
-  ## TODO-EXCEPTION
-  infile = csv.DictReader(open(os.path.expanduser(diff_file), 'r'))
-
-  res = []
-
-  for row in infile:
-    res.append(row)
-
-  return res
-
 
 def prep_questionnaire_jobs(template_file="~/.scaleupbrazil/template-ids.json",
                             svypath_file="~/.scaleupbrazil/survey-paths.csv",
